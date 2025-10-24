5b42e0b9179a48a86727e2d6dcd61466e790044d
Adriano <adriano2604@hotmail.com>
2025-10-24 17:43:05 -0300

    feat(counter): adicionado um sistema contador para criar numero da conta ao registra-la no sistema.

---
+++ models/shared/ICounter.ts
@@ -0,0 +1,5 @@
+export interface ICounter {
+  _id: string;
+  sequenceValue: number;
+};
+
---
+++ models/shared/counterSchema.ts
@@ -0,0 +1,38 @@
+import { Schema } from 'mongoose'
+import { getBankingDB } from '../../database/db/bankingDB.ts'
+import { throwlhos } from '../../global/Throwlhos.ts'
+import { ICounter } from './ICounter.ts'
+
+const counterSchema = new Schema<ICounter>({
+  _id: {
+    type: String,
+    required: true
+  },
+  sequenceValue: {
+    type: Number,
+    default: 1000000
+  },
+},{
+  _id: false,
+  timestamps: true,
+});
+
+export async function getNextAccountNumber(): Promise<number> {
+  const Counter = getBankingDB().model<ICounter>('Counter', counterSchema);
+
+  const counter = await Counter.findByIdAndUpdate(
+    'accNumber',
+    { $inc: { sequenceValue: 1 } },
+    {
+      new: true,
+      upsert: true
+    }
+  );
+
+  if (!counter) {
+    throw throwlhos.err_notFound('Falha ao gerar o número da conta');
+  }
+
+  return counter.sequenceValue;
+  
+}
