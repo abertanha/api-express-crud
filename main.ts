9e46932e5cc135c26b3435d8452af08022f38cce
Adriano <adriano2604@hotmail.com>
2025-10-24 17:51:17 -0300

    refactor(main,build): pequenas correções para se alinhar aos padrões de projeto.

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import { Env } from './config/Env.ts';
-import { Database } from './database/Database.ts';
+import { initializeBankingDB } from './database/db/bankingDB.ts'
+import { Database } from './database/Database.ts'
 
 
 const server = async () => { 
@@ -10,14 +11,14 @@ const server = async () => {
     console.log(`[Server] Environment set to env.${Env.name}`);
 
     console.log('[Server] Connecting to Database...');
-    await Database.initialize();
+    await initializeBankingDB();
     console.log('[Server] Database connection established.');
 
+    
     const app = express();
     app.use(express.json());
     app.use(morgan('dev'));
     
-
     app.get('/', (_req, res) => {
       res.send('API está rolando...');
     });
@@ -28,11 +29,25 @@ const server = async () => {
     const PORT = Env.port;
     app.listen(PORT, () => {
       console.log(`[Server] Servidor disponível na porta ${PORT}`);
+      console.log(`[Server] Conexões ativas: ${Database.listConnections().join(', ')}`);
     });
+
+    const shutdown = async () => {
+      console.log('\n[Server] Graceful shutdown initiated...');
+      await Database.closeAllConnections();
+      console.log('[Server] Shutdown complete.');
+      Deno.exit(0);
+    }
+
+    Deno.addSignalListener('SIGINT', shutdown);
+    Deno.addSignalListener('SIGTERM', shutdown);
+
   } catch (error) {
+    
     console.error('[Server] Failed to start server:');
     console.error(error);
     Deno.exit(1);
+  
   }
 };
 
